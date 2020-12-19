30f4b03d57c7ca12d7dbfe2f6f9ac443f887d40f
Chooklii <34456707+chooklii@users.noreply.github.com>
2020-12-19 17:45:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import "../static/style-mobile.css"
+import "../static/style-desktop.css"
+import "../static/style-tablet.css"
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+                        <Route exact path="/">
+                            <div>
+                                Home
+                    </div>
+                        </Route>
+
+                        <Route path="*">
+                            <div>
+                                404
+                            </div>
+                        </Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+export default App;
